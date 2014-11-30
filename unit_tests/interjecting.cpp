f9787ebb62841c72b8c6dadb7af4c3f058270882
AndrewGaspar <andrew.gaspar.git@outlook.com>
2014-11-30 11:38:27 -0800

    Added tests ensuring interjecting respects termination

--- unit_tests/interjecting.cpp
+++ unit_tests/interjecting.cpp
@@ -6,6 +6,7 @@
 #include <transducers\interjecting.hpp>
 #include <transducers\output.hpp>
 #include <transducers\compose.hpp>
+#include <transducers\taking.hpp>
 
 using namespace transducers;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -15,15 +16,36 @@ namespace unit_tests
 	TEST_CLASS(InterjectingTests)
 	{
 	public:
-		
+        std::vector<std::string> fruits;
+
+        InterjectingTests()
+        {
+            fruits = { "banana", "apple", "cherry" };
+        }
+
 		TEST_METHOD(BetweenElements)
 		{
-            std::vector<std::string> fruits{ "banana", "apple", "cherry" };
             std::ostringstream stream;
             output_to(interjecting(", "), stream, fruits);
             std::string expected = "banana, apple, cherry";
             Assert::AreEqual(expected, stream.str());
 		}
+
+        TEST_METHOD(RespectsTermination)
+        {
+            std::ostringstream stream;
+            output_to(compose(interjecting(','), taking(4)), stream, fruits);
+            std::string expected = "banana,apple,";
+            Assert::AreEqual(expected, stream.str());
+        }
+
+        TEST_METHOD(RespectsTermination2)
+        {
+            std::ostringstream stream;
+            output_to(compose(interjecting(','), taking(3)), stream, fruits);
+            std::string expected = "banana,apple";
+            Assert::AreEqual(expected, stream.str());
+        }
 
         TEST_METHOD(Nested)
         {
