ccff472c6c032ee63c6a3db60252917047b6e113
AndrewGaspar <andrew.james.gaspar@gmail.com>
2014-12-05 00:42:32 -0800

    Removed 'inplace_reduction' and use std::reference_wrapper instead.

--- include/transducers/output.hpp
+++ include/transducers/output.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
+#include <type_traits>
+
 #include "transducers/transduce.hpp"
 #include "transducers/type_traits.hpp"
-#include "transducers/inplace_reduction.hpp"
 
 namespace transducers {
     namespace details {
@@ -11,32 +12,25 @@ namespace transducers {
         {
             stored_argument_t<_Os> m_outStream;
         public:
-            using ostream_type = typename std::remove_reference<_Os>::type;
-
-            StreamReducer(_Os&& output_stream) : m_outStream(std::forward<_Os>(output_stream)) {}
-
-            inplace_reduction<ostream_type&> init()
-            {
-                return inplace_reduction<ostream_type&>(m_outStream);
-            }
+            using ostream_type = _Os;
 
             template<typename Input, typename _EsHa>
-            inplace_reduction<ostream_type&> & step(inplace_reduction<ostream_type&> & reduction, Input&& input, _EsHa&)
+            std::reference_wrapper<ostream_type> step(std::reference_wrapper<ostream_type> reduction, Input&& input, _EsHa&)
             {
-                reduction.m_reduction << std::forward<Input>(input);
+                reduction.get() << std::forward<Input>(input);
                 return reduction;
             }
 
-            std::reference_wrapper<ostream_type> complete(inplace_reduction<ostream_type&> & reduction)
+            std::reference_wrapper<ostream_type> complete(std::reference_wrapper<ostream_type> & reduction)
             {
-                return reduction.m_reduction;
+                return reduction;
             }
         };
     }
 
     template<typename _Tr, typename _Ra, typename _Os>
-    auto output_to(_Tr&& transducer, _Os&& output_stream, _Ra&& input)
+    auto output_to(_Tr&& transducer, _Os & output_stream, _Ra&& input)
     {
-        return transduce(std::forward<_Tr>(transducer), details::StreamReducer<_Os>(output_stream), std::forward<_Ra>(input), nonatomic_escape_hatch());
+        return transduce(std::forward<_Tr>(transducer), details::StreamReducer<_Os>(), std::reference_wrapper<_Os>(output_stream), std::forward<_Ra>(input), nonatomic_escape_hatch());
     }
 };
