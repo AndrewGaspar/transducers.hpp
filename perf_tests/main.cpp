10fe84d2cc37ac5221045b9dae815c43106916b2
AndrewGaspar <andrew.james.gaspar@gmail.com>
2014-12-07 00:54:17 -0800

    Added project for performance tests.

---
+++ perf_tests/main.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <chrono>
+
+#include <transducers\transduce.hpp>
+#include <transducers\mapping.hpp>
+
+int main(int count, char** args)
+{
+    std::vector<char*> arguments(args, args + count);
+
+    
+}
